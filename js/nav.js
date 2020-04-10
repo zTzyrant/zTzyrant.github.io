b5f6601b74c390a0e9c765de6df8cc76d6e20545
zTzyrant <61049884+zTzyrant@users.noreply.github.com>
2020-04-10 20:14:22 +0800

    Hmm Adding Parallax, and some bug ~

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,31 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickNav()};
 
 // Get the header
-var header = document.getElementById("myHeader");
-
+var header = document.getElementById("NavHead");
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickNav() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
 } 
+var moon = document.getElementById("moon")
+var astro = document.getElementById("astro")
+var rocket = document.getElementById("rocket")
+
+window.onmousemove = function(e){
+      var x = - e.clientX/30,
+          y = - e.clientY/30,
+          mx = - e.clientX/50,
+          my = - e.clientY/50,
+          rx = - e.clientX/10,
+          ry = - e.clientY/10;
+          moon.style.transform = "translate3d(" + x + "px" + ", " + y + "px, 0)";
+          astro.style.transform = "translate3d(" + mx + "px" + ", " + my + "px, 0)";
+          rocket.style.transform = "translate3d(" + rx + "px" + ", " + ry + "px, 0)";
+}
